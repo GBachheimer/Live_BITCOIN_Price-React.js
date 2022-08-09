1d8d15f3b2c6b99ccef87a9093a4ef095d952695
GBachheimer <gbachheimer@icloud.com>
2022-08-09 11:48:32 +0300

    all files created

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Graph from "./components/Graph"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Graph />);
